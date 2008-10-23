980870a9a5263aa26f52bbe4967c65b91ab6947b
Deepti Shelar <deepti_shelar>
2008-10-23 07:21:48 +0000

    SecurityManager : A CSM project
    git-svn-id: https://ncisvn.nci.nih.gov/svn/catissue_security_manager/SecurityManager/trunk@106 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ src/edu/wustl/common/security/PrivilegeLocator.java
@@ -0,0 +1,124 @@
+package edu.wustl.common.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import edu.wustl.common.util.global.XMLParserUtility;
+
+/**
+ * This classes load all privileges from xml file.
+ * @author ravi_kumar
+ */
+public class PrivilegeLocator
+{
+	
+	
+	/**
+	 * File name for privilege configuration.
+	 */
+	private static final String PRIV_CONF_FILE="PrivilegeConf.xml";
+
+	/**
+	 * Element name for 'privilege'.
+	 */
+	private static final String ELE_PRIVILEGE="privilege";
+
+	/**
+	 * Element name for 'bit-number'.
+	 */
+	private static final String ELE_BIT_NUM="bit-number";
+
+	/**
+	 * Element name for 'role-name'.
+	 */
+	private static final String ELE_ROLE_NAME="role-name";
+
+	/**
+	 * Attribute name for 'name'.
+	 */
+	private static final String ATTR_NAME="name";
+
+	/**
+	 * Map of privilege Name and Privilege Object.
+	 */
+	static private Map<String,Privilege> namePrivMap;
+
+	/**
+	 * Map of bit-number and Privilege Object.
+	 */
+	static private Map<Integer,Privilege> bitPrivMap;
+	
+	static{		
+		init();
+	}
+	/**
+	 * This method returns Privilege object by privilege name.
+	 * @param name Privilege Name
+	 * @return Privilege object.
+	 */
+	static public Privilege getPrivilegeByName(String name)
+	{
+		return namePrivMap.get(name);
+	}
+
+	/**
+	 * This method returns Privilege object by bit number.
+	 * @param bit bit number associated with privilege.
+	 * @return Privilege object.
+	 */
+	static public Privilege getPrivilegeByBit(Integer bit)
+	{
+		return bitPrivMap.get(bit);
+	}
+
+	/**
+	 * This method load the Privileges into map.
+	 */
+	static public void init()
+	{
+		Document doc = XMLParserUtility.getDocument(PRIV_CONF_FILE);
+		NodeList privNodeLst = doc.getElementsByTagName(ELE_PRIVILEGE);
+		populateMaps(privNodeLst);
+	}
+
+	/**
+	 * @param privNodeLst this method populate xml data to maps.
+	 */
+	static private void populateMaps(NodeList privNodeLst)
+	{
+		Node privNode;
+		namePrivMap= new HashMap<String, Privilege>();
+		bitPrivMap= new HashMap<Integer, Privilege>();
+		for (int s = 0; s < privNodeLst.getLength(); s++)
+		{
+		    privNode = privNodeLst.item(s);
+		    if (privNode.getNodeType() == Node.ELEMENT_NODE)
+		    {
+		    	addNewPrivilegeToMap(privNode);
+		    }
+		}
+	}
+
+	/**
+	 * @param privNode Node- xml privilege node
+	 */
+	static private void addNewPrivilegeToMap(Node privNode)
+	{
+	    String privName;
+		int bitNumber;
+		String roleName;
+		Privilege privilege;
+		Element privElmnt = (Element) privNode;
+	    privName=privElmnt.getAttribute(ATTR_NAME);
+	    bitNumber = Integer.parseInt(XMLParserUtility.getElementValue(privElmnt,ELE_BIT_NUM));
+	    roleName = XMLParserUtility.getElementValue(privElmnt,ELE_ROLE_NAME);
+	    privilege= new Privilege(privName,bitNumber,roleName);
+	    namePrivMap.put(privName, privilege);
+	    bitPrivMap.put(bitNumber,privilege);
+	}
+}
