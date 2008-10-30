333d742add0adc3214c6ec9c8b18462ab159c200
Deepti Shelar <deepti_shelar>
2008-10-30 14:16:28 +0000

    Adding locator classes for Security Manager conf files
    git-svn-id: https://ncisvn.nci.nih.gov/svn/catissue_security_manager/SecurityManager@108 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ trunk/src/edu/wustl/security/locator/SecurityManagerPropertiesLocator.java
@@ -0,0 +1,38 @@
+package edu.wustl.security.locator;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import edu.wustl.common.util.dbmanager.DBUtil;
+import edu.wustl.common.util.global.Constants;
+import edu.wustl.common.util.logger.Logger;
+import edu.wustl.security.manager.SecurityManager;
+
+public class SecurityManagerPropertiesLocator {
+
+	/**
+	 * logger Logger - Generic logger.
+	 */
+	private static org.apache.log4j.Logger logger = Logger.getLogger(SecurityManager.class);
+
+	private static Properties SECURITY_MANAGER_PROP;
+	public static String APPLICATION_CONTEXT_NAME = null;	
+	public static String SECURITY_MANAGER_CLASS = null;
+	public static void init() {
+		InputStream inputStream = DBUtil.class.getClassLoader().getResourceAsStream(
+				Constants.SECURITY_MANAGER_PROP_FILE);
+		SECURITY_MANAGER_PROP = new Properties();
+		try
+		{
+			SECURITY_MANAGER_PROP.load(inputStream);
+			inputStream.close();
+			APPLICATION_CONTEXT_NAME = SECURITY_MANAGER_PROP.getProperty(Constants.APPLN_CONTEXT_NAME);
+			SECURITY_MANAGER_CLASS = SECURITY_MANAGER_PROP.getProperty(Constants.SECURITY_MANAGER_CLASSNAME);
+		}
+		catch (IOException exception)
+		{
+			logger.warn("Not able to initialize Security Manager Properties.", exception);
+		}
+	}
+}
