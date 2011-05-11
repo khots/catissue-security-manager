2db41c2690fc197592a1a3090f481a5eaac4da39
Srikanth Adiga <srikanth.adiga@gmail.com>
2011-05-11 09:15:55 +0000

    merging the bug fixes for 1.2.1wu release
    git-svn-id: https://ncisvn.nci.nih.gov/svn/catissue_security_manager/SecurityManager@342 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/https:/ncisvn.nci.nih.gov/svn/catissue_security_manager/SecurityManager/branches/SecurityManager_1.2.1.wu/src/edu/wustl/security/impl/AuthorizationDAOImpl.java
+++ branches/https:/ncisvn.nci.nih.gov/svn/catissue_security_manager/SecurityManager/branches/SecurityManager_1.2.1.wu/src/edu/wustl/security/impl/AuthorizationDAOImpl.java
@@ -24,7 +24,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import edu.wustl.security.global.Constants;
-
 import gov.nih.nci.logging.api.logger.hibernate.HibernateSessionFactoryHelper;
 import gov.nih.nci.security.authorization.ObjectPrivilegeMap;
 import gov.nih.nci.security.authorization.domainobjects.Group;
@@ -300,10 +299,10 @@ public class AuthorizationDAOImpl extends gov.nih.nci.security.dao.Authorization
 
 				" and pe.attribute " + attributeVal
 				+ " and pg.protection_group_id = ugrpg.protection_group_id "
-				+ "and pg.APPLICATION_ID=? and (( ugrpg.group_id = g.group_id" + " and ug.group_id= g.group_id"
+				+ "and pg.APPLICATION_ID=? and ( ugrpg.group_id = g.group_id" + " and ug.group_id= g.group_id"
 				+ "       and ug.user_id = u.user_id)" + " and u.login_name=?"
 				+ " and ugrpg.role_id = rp.role_id " + " and rp.privilege_id = p.privilege_id";
-		
+
 		stbr.append(str);
 	}
 }
