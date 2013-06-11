f3db36a9211bae5d5fb6f6f6687e4e5b82f739f0
Prasad Konka <prasad.konka@nih.gov>
2013-06-11 13:15:35 -0400

    Added license header

--- software/target/codegen/system/src/gov/nih/nci/system/dao/orm/ORMDAOImpl.java
+++ software/target/codegen/system/src/gov/nih/nci/system/dao/orm/ORMDAOImpl.java
@@ -1,3 +1,13 @@
+/*L
+ *  Copyright Washington University in St. Louis
+ *  Copyright SemanticBits
+ *  Copyright Persistent Systems
+ *  Copyright Krishagni
+ *
+ *  Distributed under the OSI-approved BSD 3-Clause License.
+ *  See http://ncip.github.com/catissue-security-manager/LICENSE.txt for details.
+ */
+
 package gov.nih.nci.system.dao.orm;
 
 import gov.nih.nci.system.dao.DAO;
